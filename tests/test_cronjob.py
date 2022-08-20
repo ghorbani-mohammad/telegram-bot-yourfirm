c99d074b2ab2e30c4a3bda1ed5db06d186627ab8
Mohammad <mohammad.ghorbani@hotmail.com>
2022-08-20 17:25:33 +0430

    add comment

--- tests/test_cronjob.py
+++ tests/test_cronjob.py
@@ -6,7 +6,9 @@ from api.models import Profile, Subscription
 
 
 @pytest.mark.django_db
-def test_subscribe(client):
+def test_subscribe():
+    # In this test we have not assertion, Hopefully if we have any error
+    # We will get an exception
     profile = Profile.objects.create(user_id=settings.TEST_ACCOUNT_ID)
     Subscription.objects.create(profile=profile, term='python')
     Crawler.crawl()
