4bd5fc92da66710647fa32dcc9f1d59d998fd34a
Mohammad (Mike) Ghorbani <mohammad.ghorbani@hotmail.com>
2022-08-23 02:25:04 +0430

    Revert "add tests"

--- tests/test_yourfirm.py
+++
@@ -1,9 +0,0 @@
-
-from api.yourfirm import Yourfirm
-
-
-def test_yourfirm_api():
-    # in the good situation, we should get response
-    # and in the response we have a result field which has
-    # greater or equal to zero items
-    assert len(Yourfirm.search(term='python')['result']) >= 0
