65f61d99c5b0bb508a065c7659a327709463c96b
Mohammad <mohammad.ghorbani@hotmail.com>
2022-08-22 15:47:41 +0430

    add unique to user-id field of Profile

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user_id = models.CharField(max_length=20, blank=True, null=True)
+    user_id = models.CharField(max_length=20, blank=True, null=True, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
