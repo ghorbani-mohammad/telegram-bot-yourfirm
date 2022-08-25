c549bec9b13be404777e523864c5b32aab5b01e1
Mohammad <mohammad.ghorbani@hotmail.com>
2022-08-25 16:02:03 +0430

    use simple method for YF search

--- api/yourfirm.py
+++ api/yourfirm.py
@@ -1,14 +1,11 @@
 import requests
-import urllib.parse as up
+import urllib.parse
 
 from django.conf import settings
 
 
-class Yourfirm():
-
-    @staticmethod
-    def search(term):
-        term = up.quote(term, safe='')
-        url = f'{settings.YOURFIRM_URL}/api/jobs/?name={term}'
-        r = requests.get(url)
-        return r.json()
+def yourfirm_search(term):
+    term = urllib.parse.quote(term, safe='')
+    url = f'{settings.YOURFIRM_URL}/api/jobs/?name={term}'
+    r = requests.get(url)
+    return r.json()
