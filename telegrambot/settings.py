35851b8e0b4661ee4631deb3de421e86b6c070d9
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-27 19:14:24 +0330

    minor change

--- telegrambot/settings.py
+++ telegrambot/settings.py
@@ -7,7 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1nv*-1bq#6=b3j2&$kritu^g)g0_a7zk9h5cy=t-49o-8v&=^w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
