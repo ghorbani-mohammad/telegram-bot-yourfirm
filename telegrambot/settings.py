cb4682f66768e5f42b345647ee1ccf8717e80a70
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-27 19:15:58 +0330

    minor change

--- telegrambot/settings.py
+++ telegrambot/settings.py
@@ -55,9 +55,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'telegrambot.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
