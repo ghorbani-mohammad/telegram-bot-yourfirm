5607555bc1e856eebffcf70de83b01fce21bfed6
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-27 19:14:15 +0330

    minor change

--- telegrambot/settings.py
+++ telegrambot/settings.py
@@ -7,9 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1nv*-1bq#6=b3j2&$kritu^g)g0_a7zk9h5cy=t-49o-8v&=^w'
 
